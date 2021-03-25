744be9b81d71b83b679e9e67d6648c4f6956606b
M-Horvathova <72359428+M-Horvathova@users.noreply.github.com>
2021-03-25 14:46:51 +0100

    In-memory database init done (#14)
    Correct derby dependency setId
    Includes playground for database inserts fast testing

---
+++ src/main/java/energySystem/Main.java
@@ -0,0 +1,43 @@
+package energySystem;
+
+import energySystem.entity.MeterLog;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.time.LocalDate;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    // Playgroung method for very basic testing if the database works
+    public static void playground() {
+        EntityManagerFactory emf = null;
+        EntityManager em = null;
+
+        emf = Persistence.createEntityManagerFactory("default");
+        em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            MeterLog ml = new MeterLog();
+            ml.setLogDate(LocalDate.of(2021, 1, 13).atTime(1, 30, 0));
+            ml.setMeasure(123L);
+            em.persist(ml);
+
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext(InMemoryDatabaseSpring.class);
+
+        playground();
+        System.out.println("Hello World");
+    }
+}
