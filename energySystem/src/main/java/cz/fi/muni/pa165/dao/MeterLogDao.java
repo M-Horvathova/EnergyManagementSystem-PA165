885efdfa2564470337786c45fbe1ce5ae98d4f42
Michaela Horvathova <456285@mail.muni.cz>
2021-03-26 20:08:44 +0100

    proper project structure done

---
+++ energySystem/src/main/java/cz/fi/muni/pa165/dao/MeterLogDao.java
@@ -0,0 +1,21 @@
+package cz.fi.muni.pa165.dao;
+
+import cz.fi.muni.pa165.entity.MeterLog;
+import cz.fi.muni.pa165.enums.dayTime;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+/**
+ * @author Michaela Horváthová
+ */
+public interface MeterLogDao {
+    public MeterLog findById(Long id);
+    public void create(MeterLog ml);
+    public void delete(MeterLog ml);
+    public List<MeterLog> findAll();
+    public List<MeterLog> findByDate(LocalDate date);
+    public List<MeterLog> findByTimeOfDay(LocalDate date, dayTime dayTime);
+
+}
---
+++ energySystem/src/main/java/cz/fi/muni/pa165/enums/dayTime.java
@@ -0,0 +1,8 @@
+package cz.fi.muni.pa165.enums;
+
+/**
+ * @author Michaela Horváthová
+ */
+public enum dayTime {
+    night, day
+}
