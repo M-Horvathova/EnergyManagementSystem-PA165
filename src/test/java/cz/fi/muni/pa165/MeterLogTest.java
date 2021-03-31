289a25b0153612082d7f48ee6b2929af6afc2ddd
Patrik Valo <xvalo2@fi.muni.cz>
2021-03-31 23:09:27 +0200

    Advanced tests for MeterLogDao added

--- src/test/java/cz/fi/muni/pa165/MeterLogTest.java
+++ src/test/java/cz/fi/muni/pa165/MeterLogTest.java
@@ -3,10 +3,11 @@ package cz.fi.muni.pa165;
 import cz.fi.muni.pa165.dao.MeterLogDao;
 import cz.fi.muni.pa165.entity.MeterLog;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.jpa.JpaSystemException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import javax.persistence.EntityManager;
@@ -24,6 +25,21 @@ public class MeterLogTest extends AbstractTestNGSpringContextTests {
 
     @PersistenceUnit
     private EntityManagerFactory emf;
+
+    @AfterMethod
+    public void afterTest() {
+        EntityManager em = null;
+        try {
+            em = emf.createEntityManager();
+            em.getTransaction().begin();
+            em.createQuery("delete from MeterLog").executeUpdate();
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
 
     @Test
     public void basicCreateTest() {
@@ -44,6 +60,50 @@ public class MeterLogTest extends AbstractTestNGSpringContextTests {
         Assert.assertEquals(result.getLogDate(), date);
         Assert.assertEquals(result.getLogTime(), time);
         Assert.assertEquals(result.getMeasure(), measure);
+    }
+
+    @Test(expectedExceptions = JpaSystemException.class)
+    public void nullableDateCreateTest() {
+        var time = LocalTime.of(15, 30);
+        Long measure = 123L;
+
+        MeterLog meterLog = new MeterLog();
+        meterLog.setLogTime(time);
+        meterLog.setMeasure(measure);
+
+        meterLogDao.create(meterLog);
+    }
+
+    @Test(expectedExceptions = JpaSystemException.class)
+    public void nullableTimeCreateTest() {
+        var date = LocalDate.of(2021, 1, 23);
+        Long measure = 123L;
+
+        MeterLog meterLog = new MeterLog();
+        meterLog.setLogDate(date);
+        meterLog.setMeasure(measure);
+
+        meterLogDao.create(meterLog);
+    }
+
+    @Test(expectedExceptions = JpaSystemException.class)
+    public void nullableMeasureCreateTest() {
+        var date = LocalDate.of(2021, 1, 23);
+        var time = LocalTime.of(15, 30);
+
+        MeterLog meterLog = new MeterLog();
+        meterLog.setLogDate(date);
+        meterLog.setLogTime(time);
+
+        meterLogDao.create(meterLog);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void setFutureDateTest() {
+        var date = LocalDate.now().plusDays(1);
+
+        MeterLog meterLog = new MeterLog();
+        meterLog.setLogDate(date);
     }
 
     @Test
@@ -121,6 +181,19 @@ public class MeterLogTest extends AbstractTestNGSpringContextTests {
     }
 
     @Test
+    public void findNotExistingMeterLogTest() {
+        var date = LocalDate.of(2016, 12, 15);
+        var time = LocalTime.of(0, 0);
+        Long measure = 11L;
+
+        MeterLog meterLog = createEntityMeterLog(date, time, measure);
+        meterLogDao.delete(meterLog);
+
+        MeterLog result = meterLogDao.findMeterLog(meterLog);
+        Assert.assertNull(result);
+    }
+
+    @Test
     public void findByIdTest() {
         var date = LocalDate.of(2016, 12, 31);
         var time = LocalTime.of(0, 30);
@@ -133,6 +206,12 @@ public class MeterLogTest extends AbstractTestNGSpringContextTests {
         Assert.assertEquals(result.getLogDate(), date);
         Assert.assertEquals(result.getLogTime(), time);
         Assert.assertEquals(result.getMeasure(), measure);
+    }
+
+    @Test
+    public void findByNotExistingIdTest() {
+        MeterLog result = meterLogDao.findById(16L);
+        Assert.assertNull(result);
     }
 
     @Test
@@ -157,19 +236,11 @@ public class MeterLogTest extends AbstractTestNGSpringContextTests {
         Assert.assertEquals(list.size(), 3, "Length of the list should be same");
     }
 
-    @AfterTest
-    public void afterTest() {
-        EntityManager em = null;
-        try {
-            em = emf.createEntityManager();
-            em.getTransaction().begin();
-            em.createQuery("delete from MeterLog").executeUpdate();
-            em.getTransaction().commit();
-        } finally {
-            if (em != null) {
-                em.close();
-            }
-        }
+    @Test
+    public void findAllEmptyTest() {
+        List<MeterLog> list = meterLogDao.findAll();
+        Assert.assertNotNull(list);
+        Assert.assertEquals(list.size(), 0, "Length of the list should be same");
     }
 
     private MeterLog findEntityInDb(Long id) {
