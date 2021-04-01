50a5394e147d2eb20d388488cd3e1fcad9b64b36
Matej Risnovsky <matorzh@gmail.com>
2021-04-01 23:05:44 +0200

    Smart meter entity + dao

---
+++ src/main/java/cz/fi/muni/pa165/dao/SmartMeterDaoImpl.java
@@ -0,0 +1,49 @@
+package cz.fi.muni.pa165.dao;
+
+import cz.fi.muni.pa165.entity.MeterLog;
+import cz.fi.muni.pa165.entity.SmartMeter;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class SmartMeterDaoImpl implements SmartMeterDao {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void create(SmartMeter smartMeter) {
+        em.persist(smartMeter);
+    }
+
+    @Override
+    public SmartMeter update(SmartMeter smartMeter) {
+        return em.merge(smartMeter);
+    }
+
+    @Override
+    public SmartMeter findById(Long id) {
+        SmartMeter sm = em.find(SmartMeter.class, id);
+        return sm;
+    }
+
+    @Override
+    public List<SmartMeter> findAll() {
+        List<SmartMeter> smartMeters = em.createQuery("select sm from SmartMeter sm", SmartMeter.class).getResultList();
+        return smartMeters;
+    }
+
+    @Override
+    public List<SmartMeter> findByRunning(boolean running) {
+        List<SmartMeter> smartMeters = em.createQuery("select sm from SmartMeter sm where sm.isRunning = :running", SmartMeter.class).getResultList();
+        return smartMeters;
+    }
+
+    @Override
+    public void delete(SmartMeter smartMeter) {
+        smartMeter = em.merge(smartMeter);
+        em.remove(smartMeter);
+    }
+}
