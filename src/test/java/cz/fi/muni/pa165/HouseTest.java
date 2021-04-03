31bd7989cd978710ee4c92a7887a4d5a5df7e97d
Michaela Horvathova <456285@mail.muni.cz>
2021-04-03 11:30:49 +0200

    test basic setup of class

---
+++ src/test/java/cz/fi/muni/pa165/HouseTest.java
@@ -0,0 +1,51 @@
+package cz.fi.muni.pa165;
+
+import cz.fi.muni.pa165.dao.HouseDao;
+import cz.fi.muni.pa165.entity.Address;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+
+/**
+ * @author Michaela Horváthová
+ */
+@ContextConfiguration(classes = PersistenceApplicationContext.class)
+public class HouseTest extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private HouseDao houseDao;
+
+    @PersistenceUnit
+    private EntityManagerFactory emf;
+
+    @AfterMethod
+    public void afterTest() {
+        EntityManager em = null;
+        try {
+            em = emf.createEntityManager();
+            em.getTransaction().begin();
+            em.createQuery("delete from House ").executeUpdate();
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    @Test
+    public void basicCreateTest() {
+        Address address = new Address();
+        address.setStreetName("Botanická");
+        address.setCity("Brno");
+        address.setCountry("Czech Republic");
+    }
+
+
+}
