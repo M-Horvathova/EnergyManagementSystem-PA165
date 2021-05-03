22238f110f0c711efc840818b389ab3dbab5595f
Michaela Horvathova <456285@mail.muni.cz>
2021-05-03 22:13:35 +0200

    yarn start works, cargo:run works

---
+++ web/src/main/java/cz/fi/muni/pa165/restapi/config/RestSpringReact.java
@@ -0,0 +1,54 @@
+package cz.fi.muni.pa165.restapi.config;
+
+import cz.fi.muni.pa165.service.config.ServiceConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import javax.validation.Validator;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
+@Configuration
+@EnableWebMvc
+@Import({ServiceConfiguration.class})
+@ComponentScan(basePackages = {"cz.fi.muni.pa165.restapi.*"})
+public class RestSpringReact implements WebMvcConfigurer{
+
+    @Bean
+    public MappingJackson2HttpMessageConverter customJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        jsonConverter.setObjectMapper(objectMapper());
+        return jsonConverter;
+    }
+
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(customJackson2HttpMessageConverter());
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        //configuring mapper for HAL
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH));
+        return objectMapper;
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer c) {
+        c.defaultContentType(MediaTypes.HAL_JSON);
+    }
+
+   }
