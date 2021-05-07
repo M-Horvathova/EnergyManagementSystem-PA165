1306f7da4c5f4c562405d987ccc6373d5bc8cfaf
Patrik Valo <xvalo2@fi.muni.cz>
2021-05-07 23:09:48 +0200

    Fix rest config to work

--- rest/src/main/java/cz/fi/muni/pa165/restapi/config/RestSpringMvcConfig.java
+++ rest/src/main/java/cz/fi/muni/pa165/restapi/config/RestSpringMvcConfig.java
@@ -1,19 +1,16 @@
 package cz.fi.muni.pa165.restapi.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import cz.muni.fi.pa165.sampledata.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.*;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 import java.text.SimpleDateFormat;
 import java.util.List;
@@ -22,7 +19,6 @@ import java.util.Locale;
 import static org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
 
 
-@EnableHypermediaSupport(type = HypermediaType.HAL)
 @EnableWebMvc
 @Configuration
 @Import({SampleDataConfiguration.class})
@@ -34,10 +30,23 @@ public class RestSpringMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(new AllowOriginInterceptor());
     }
 
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
     @Bean
+    @Primary
     public MappingJackson2HttpMessageConverter customJackson2HttpMessageConverter() {
         MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
-        jsonConverter.setObjectMapper(objectMapper());
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.ENGLISH));
+
+        objectMapper.disable(MapperFeature.DEFAULT_VIEW_INCLUSION);
+
+        jsonConverter.setObjectMapper(objectMapper);
         return jsonConverter;
     }
 
@@ -45,21 +54,4 @@ public class RestSpringMvcConfig implements WebMvcConfigurer {
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(customJackson2HttpMessageConverter());
     }
-
-    // see  http://stackoverflow.com/questions/25709672/how-to-change-hal-links-format-using-spring-hateoas
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer c) {
-        c.defaultContentType(MediaTypes.HAL_JSON);
-    }
-
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        //configuring mapper for HAL
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH));
-        return objectMapper;
-    }
-
-
 }
