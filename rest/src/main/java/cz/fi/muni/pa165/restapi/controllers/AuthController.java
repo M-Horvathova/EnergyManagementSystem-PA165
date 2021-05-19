015f5071567d4ed04ae9edc57ebf90178684cab6
Martin Podhora <martin.podhora@puxdesign.cz>
2021-05-19 07:37:43 +0200

    Fix cors policy

--- rest/src/main/java/cz/fi/muni/pa165/restapi/controllers/AuthController.java
+++ rest/src/main/java/cz/fi/muni/pa165/restapi/controllers/AuthController.java
@@ -12,7 +12,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
 public class AuthController {
 
     private final PortalUserFacade portalUserFacade;
